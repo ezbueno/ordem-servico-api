abeb858f390de0349516095ff53271fd290bba8c
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-10 17:17:34 -0300

    Repository

---
+++ src/main/java/com/ezandro/ordemservico/api/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package com.ezandro.ordemservico.api.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ezandro.ordemservico.api.entity.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
+}
