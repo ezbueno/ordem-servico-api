a298ccd7fc2841431af0352e22ce3f81f943fbdf
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-10 17:30:40 -0300

    Insert validation

--- src/main/java/com/ezandro/ordemservico/api/controller/ClienteController.java
+++ src/main/java/com/ezandro/ordemservico/api/controller/ClienteController.java
@@ -3,6 +3,8 @@ package com.ezandro.ordemservico.api.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -44,12 +46,12 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long id, @RequestBody Cliente cliente) {
 		
 		if(!clienteRepository.existsById(id)) {
 			return ResponseEntity.notFound().build();
