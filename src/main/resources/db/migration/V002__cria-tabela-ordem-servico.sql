4ce284a1b39eb86dc399f6eabd7870ad27245a99
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-11 22:22:06 -0300

    Created table ordem_servico

---
+++ src/main/resources/db/migration/V002__cria-tabela-ordem-servico.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS ordem_servico(
+	id BIGINT NOT NULL AUTO_INCREMENT,
+	cliente_id BIGINT NOT NULL,
+	descricao TEXT NOT NULL,
+	preco DECIMAL(10,2) NOT NULL,
+	status VARCHAR(20) NOT NULL,
+	data_abertura DATETIME NOT NULL,
+	data_finalizacao DATETIME,
+	
+	PRIMARY KEY (id),
+	CONSTRAINT fk_ordem_servico_cliente FOREIGN KEY (cliente_id) REFERENCES cliente (id)
+);
