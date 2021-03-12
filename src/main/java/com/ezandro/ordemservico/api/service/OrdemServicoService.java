73752aaa1c205707e0e7b78149eef1ee710b1c4b
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-12 15:29:42 -0300

    Service

--- src/main/java/com/ezandro/ordemservico/api/service/OrdemServicoService.java
+++ src/main/java/com/ezandro/ordemservico/api/service/OrdemServicoService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ezandro.ordemservico.api.entity.Cliente;
+import com.ezandro.ordemservico.api.entity.Comentario;
 import com.ezandro.ordemservico.api.entity.OrdemServico;
 import com.ezandro.ordemservico.api.entity.StatusOrdemServico;
 import com.ezandro.ordemservico.api.exception.NegocioException;
 import com.ezandro.ordemservico.api.repository.ClienteRepository;
+import com.ezandro.ordemservico.api.repository.ComentarioRepository;
 import com.ezandro.ordemservico.api.repository.OrdemServicoRepository;
 
 @Service
@@ -21,6 +23,9 @@ public class OrdemServicoService {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	@Autowired
+	private ComentarioRepository comentarioRepository;
+	
 	public OrdemServico criar(OrdemServico ordemServico) {
 		Cliente cliente = clienteRepository.findById(ordemServico.getCliente().getId())
 				.orElseThrow(() -> new NegocioException("Cliente não encontrado!"));
@@ -30,5 +35,17 @@ public class OrdemServicoService {
 		ordemServico.setDataAbertura(OffsetDateTime.now());
 		
 		return ordemServicoRepository.save(ordemServico);
+	}
+	
+	public Comentario adicionarComentario(Long ordemServicoId, String descricao) {
+		OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId)
+				.orElseThrow(() -> new NegocioException("Ordem de Serviço não encontrada!"));
+		
+		Comentario comentario = new Comentario();
+		comentario.setDataEnvio(OffsetDateTime.now());
+		comentario.setDescricao(descricao);
+		comentario.setOrdemServico(ordemServico);
+		
+		return comentarioRepository.save(comentario);
 	}
 }
