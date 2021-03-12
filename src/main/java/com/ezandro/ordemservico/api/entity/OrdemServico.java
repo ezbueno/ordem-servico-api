62ab1c78405438deeb10e3afcef378e6aadeda30
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-12 00:03:43 -0300

    Added Validation Groups

--- src/main/java/com/ezandro/ordemservico/api/entity/OrdemServico.java
+++ src/main/java/com/ezandro/ordemservico/api/entity/OrdemServico.java
@@ -1,7 +1,7 @@
 package com.ezandro.ordemservico.api.entity;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,6 +10,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
+
+import com.ezandro.ordemservico.api.validation.ValidationGroups;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -26,16 +35,26 @@ public class OrdemServico {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Valid
+	@ConvertGroup(from = Default.class, to =  ValidationGroups.ClienteId.class)
+	@NotNull
 	@ManyToOne
 	private Cliente cliente;
 	
+	@NotBlank
 	private String descricao;
+	
+	@NotNull
 	private BigDecimal preco;
 	
+	@JsonProperty(access = Access.READ_ONLY)
 	@Enumerated(EnumType.STRING)
 	private StatusOrdemServico status;
 	
-	private LocalDateTime dataAbertura;
-	private LocalDateTime dataFinalizacao;
+	@JsonProperty(access = Access.READ_ONLY)
+	private OffsetDateTime dataAbertura;
+	
+	@JsonProperty(access = Access.READ_ONLY)
+	private OffsetDateTime dataFinalizacao;
 	
 }
